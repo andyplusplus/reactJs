10f7931c147468aed938bba097c28f598257baf3
andyplusplus <wuzengde@vip.sina.com>
2016-08-28 18:59:47 +0800

    08/28/2016 Sun - 18:59:47.23: done with 49 - reducers and actions review

---
+++ modern_React_withRedux/ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'javascript', pages: 101 },
+		{title: 'Harry Potter', pages: 102 },
+		{title: 'The Dark Tower', pages: 103 },
+		{title: 'Eloquent Ruby', pages: 104 }
+	];
+}
